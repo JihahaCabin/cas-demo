5bd9b0bf130fce6dbf69e9928eaa59886467a4de
zhengzhendu <zdzhengzhendu@163.com>
2020-08-28 11:00:35 +0800

    修改结构

--- cas-client-A/src/main/java/com/haha/casmember/config/LocalUserInfoFilter.java
+++ cas-client-A/src/main/java/com/haha/casmember/config/LocalUserInfoFilter.java
@@ -1,6 +1,9 @@
 package com.haha.casmember.config;
 
 
+import org.jasig.cas.client.authentication.AttributePrincipal;
+import org.jasig.cas.client.util.AbstractCasFilter;
+import org.jasig.cas.client.validation.Assertion;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
@@ -24,7 +27,7 @@ public class LocalUserInfoFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request_ = (HttpServletRequest) request;
-        String loginName = CASUtil.getAccountNameFromCas(request_);
+        String loginName = getAccountNameFromCas(request_);
         if (!StringUtils.isEmpty(loginName)) {
             request_.getSession().setAttribute("loginName", loginName);
         }
@@ -34,5 +37,19 @@ public class LocalUserInfoFilter implements Filter {
     @Override
     public void destroy() {
 
+    }
+
+    /**
+     * 从cas中获取用户名
+     *
+     * @param request
+     * @return
+     */
+    private static String getAccountNameFromCas(HttpServletRequest request) {
+        Assertion assertion = (Assertion) request.getSession().getAttribute(AbstractCasFilter.CONST_CAS_ASSERTION);
+        if (assertion != null) {
+            AttributePrincipal principal = assertion.getPrincipal();
+            return principal.getName();
+        } else return null;
     }
 }
